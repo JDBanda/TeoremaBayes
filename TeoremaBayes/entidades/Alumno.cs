7afb4392523ba36bb457df98056b76d93faa3649
JDBanda <jbandav@itsoeh.edu.mx>
2020-10-10 19:57:48 -0500

    Creación de las entidades alumno y materia

---
+++ TeoremaBayes/entidades/Alumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeoremaBayes.entidades
+{
+    class Alumno
+    {
+        public string nombre { get; set; }
+        public List<Materia> listaMaterias { get; set; }
+
+        public Alumno(string nombre, List<Materia> listaMaterias)
+        {
+            this.nombre = nombre;
+            this.listaMaterias = listaMaterias;
+        }
+    }
+}
---
+++ TeoremaBayes/entidades/Materia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeoremaBayes.entidades
+{
+    class Materia
+    {
+        public string nombre { get; set; }
+        public double parcial1 { get; set; }
+        public double parcial2 { get; set; }
+        public double parcial3 { get; set; }
+        public double promedio { get; set; }
+
+        public Materia(string nombre, double parcial1, double parcial2, double parcial3)
+        {
+            this.nombre = nombre;
+            this.parcial1 = parcial1;
+            this.parcial2 = parcial2;
+            this.parcial3 = parcial3;
+        }
+    }
+}
